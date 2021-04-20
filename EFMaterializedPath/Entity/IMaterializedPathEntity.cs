1224b26b13dcebde9ee67a6c439fbbfa8ad05379
meyhem <meyhem@protonmail.com>
2021-04-20 20:31:02 +0200

    nuget, docs

--- EFMaterializedPath/Entity/IMaterializedPathEntity.cs
+++ EFMaterializedPath/Entity/IMaterializedPathEntity.cs
@@ -2,9 +2,24 @@
 {
     public interface IMaterializedPathEntity
     {
+        /// <summary>
+        /// Entity identifier to be used as PK
+        /// </summary>
         public int Id { get; set; }
+        
+        /// <summary>
+        /// Path representing materialized path of entity, empty if root
+        /// </summary>
         public string Path { get; set; }
+        
+        /// <summary>
+        /// Level of this entity in tree, 0 if root
+        /// </summary>
         public int Level { get; set; }
+        
+        /// <summary>
+        /// Reference to parent node, null if root
+        /// </summary>
         public int? ParentId { get; set; }
     }
 }
---
+++ EFMaterializedPath/Entity/MaterializedEntityMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EFMaterializedPath.Entity
+{
+    /// <summary>
+    /// Class to provide basic entity configuration for tree entities
+    /// </summary>
+    /// <typeparam name="TEntity">Entity implementing <see cref="IMaterializedPathEntity"/></typeparam>
+    public class MaterializedEntityMapping<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : class, IMaterializedPathEntity 
+    {
+        public void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(ent => ent.Id);
+            builder.Property(ent => ent.Path).IsRequired();
+            builder.Property(ent => ent.Level).IsRequired();
+            builder.Property(ent => ent.ParentId).IsRequired(false);
+
+            builder.HasIndex(ent => ent.Path).IsUnique(false);
+            builder.HasIndex(ent => ent.ParentId).IsUnique(false);
+        }
+    }
+}
